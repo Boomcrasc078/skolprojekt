9b215d3e09918d93252878c908b10f125c09196f
CertifiedArabi <ramin.ghobadi96@gmail.com>
2025-12-17 14:32:20 +0100

    Add main content section with introductory text and buttons for sign-up and log-in

--- index.php
+++ index.php
@@ -18,6 +18,24 @@
     <?php include "Components/navbar.php" ?>
 
     <main>
+        <div class="container mt-3">
+            <div class="px-4 py-5 my-5 text-center"> <img class="d-block mx-auto mb-4"
+                    src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+                <h1 class="display-5 fw-bold text-body-emphasis">Quis, Study rehearsal made easy</h1>
+                <div class="col-lg-6 mx-auto">
+
+                    <p class="lead mb-4">Make it easier to study and rehearse for tests with flashcards, quizzes and
+                        more. All using Quis!
+                    </p>
+
+                    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center"> <button type="button"
+                            class="btn btn-primary btn-lg px-4 gap-3">Sign up</button> <button type="button"
+                            class="btn btn-outline-secondary btn-lg px-4">Log in</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 
     </main>
 
