45170f983f4d3e24bde68917f8c0cb9405811379
CertifiedArabi <ramin.ghobadi96@gmail.com>
2025-12-17 16:21:27 +0100

    Update startPage component layout and content

--- Components/startPage.php
+++ Components/startPage.php
@@ -1,18 +1,75 @@
- <div class="container mt-3">
-            <div class="px-4 py-5 my-5 text-center"> <img class="d-block mx-auto mb-4"
-                    src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-                <h1 class="display-5 fw-bold text-body-emphasis">Quis, Study rehearsal made easy</h1>
-                <div class="col-lg-6 mx-auto">
+<div class="mt-3 container-fluid">
 
-                    <p class="lead mb-4">Make it easier to study and rehearse for tests with flashcards, quizzes and
-                        more. All using Quis!
-                    </p>
 
-                    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center"> <button type="button"
-                            class="btn btn-primary btn-lg px-4 gap-3">Sign up</button> <button type="button"
-                            class="btn btn-outline-secondary btn-lg px-4">Log in</button>
-                    </div>
+
+    <div class="px-4 py-5 my-5 text-center"> <img class="d-block mx-auto mb-4"
+            src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+        <h1 class="display-5 fw-bold text-body-emphasis">Quis, Study rehearsal made easy</h1>
+        <div class="col-lg-6 mx-auto">
+
+            <p class="lead mb-4">Make it easier to study and rehearse for tests with flashcards, quizzes and
+                more. All using Quis!
+            </p>
+
+            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center"> <button type="button"
+                    class="btn btn-primary btn-lg px-4 gap-3">Sign up</button> <button type="button"
+                    class="btn btn-outline-secondary btn-lg px-4">Log in</button>
+            </div>
+
+
+
+        </div>
+    </div>
+    <!--Featured Title Showcase-->
+    <div class="container px-4 py-5" id="featured-3">
+        <h2 class="pb-2 border-bottom">Columns with icons</h2>
+        <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+            <div class="feature col">
+                <div
+                    class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                    <svg class="bi" width="1em" height="1em" aria-hidden="true">
+                        <use xlink:href="#collection"></use>
+                    </svg>
                 </div>
+                <h3 class="fs-2 text-body-emphasis">Featured title</h3>
+                <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence
+                    and probably just keep going until we run out of words.</p> <a href="#" class="icon-link">
+                    Call to action
+                    <svg class="bi" aria-hidden="true">
+                        <use xlink:href="#chevron-right"></use>
+                    </svg> </a>
+            </div>
+            <div class="feature col">
+                <div
+                    class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                    <svg class="bi" width="1em" height="1em" aria-hidden="true">
+                        <use xlink:href="#people-circle"></use>
+                    </svg>
+                </div>
+                <h3 class="fs-2 text-body-emphasis">Featured title</h3>
+                <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence
+                    and probably just keep going until we run out of words.</p> <a href="#" class="icon-link">
+                    Call to action
+                    <svg class="bi" aria-hidden="true">
+                        <use xlink:href="#chevron-right"></use>
+                    </svg> </a>
+            </div>
+            <div class="feature col">
+                <div
+                    class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+                    <svg class="bi" width="1em" height="1em" aria-hidden="true">
+                        <use xlink:href="#toggles2"></use>
+                    </svg>
+                </div>
+                <h3 class="fs-2 text-body-emphasis">Featured title</h3>
+                <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence
+                    and probably just keep going until we run out of words.</p> <a href="#" class="icon-link">
+                    Call to action
+                    <svg class="bi" aria-hidden="true">
+                        <use xlink:href="#chevron-right"></use>
+                    </svg> </a>
             </div>
         </div>
+    </div>
 
+</div>
