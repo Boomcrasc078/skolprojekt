1e13298b6a323ba0e6dbe138b7b15d41f965709e
Boomcrasc078 <mose.g07@gmail.com>
2025-12-17 14:29:38 +0100

    Remove old User class and implement user handling functions for sign-up and sign-in

---
+++ Components/userHandler.php
@@ -0,0 +1,155 @@
+<?php
+session_start();
+$user = getUser();
+
+
+
+function getUser(){
+    if (!isset($_SESSION['userID'])) {
+        return;
+    }
+    $userID = $_SESSION['userID'];
+    $foundUser = find("users", "userID", $userID);
+
+    if ($foundUser == null) {
+        $_SESSION['userID'] = null;
+        return;
+    }
+
+    $user = new User($foundUser['username'], $foundUser['password']);
+    return $user;
+}
+
+class User
+{
+    public $username;
+    public $password;
+
+    function __construct($username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+    }
+}
+
+function connectToDatabase()
+{
+    $serverName = "localhost";
+    $serverUsername = "root";
+    $serverPassword = "";
+
+    $databaseName = "QUIS";
+
+    $connection = new mysqli($serverName, $serverUsername, $serverPassword, $databaseName);
+
+    if ($connection->connect_errno) {
+        throw new Exception('Database connection failed: ' . $connection->connect_error);
+    }
+
+    return $connection;
+}
+
+function query(mysqli $connection, string $query)
+{
+
+    $stmt = $connection->prepare($query);
+
+    if ($stmt === false) {
+        throw new Exception('Prepare failed: ' . $connection->error);
+    }
+
+    return $stmt;
+}
+
+
+
+function createUser(User $user)
+{
+    try {
+        $connection = connectToDatabase();
+        $hashedPassword = password_hash($user->password, PASSWORD_DEFAULT);
+
+        $stmt = query($connection, "INSERT INTO users (username, password) VALUES (?, ?)");
+        $stmt->bind_param("ss", $user->username, $hashedPassword);
+        $stmt->execute();
+
+        $userID = $connection->insert_id;
+
+        $stmt->close();
+        $connection->close();
+
+        return $userID;
+    } catch (Exception $exception) {
+        return "Error: " . $exception->getMessage();
+    }
+}
+
+function find(string $table, string $column, string $data)
+{
+    try {
+        $connection = connectToDatabase();
+
+        $stmt = query($connection, "SELECT * FROM $table WHERE $column=?");
+        $stmt->bind_param("s", $data);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        $data = $result ? $result->fetch_assoc() : null;
+
+        $stmt->close();
+        $connection->close();
+
+        return $data;
+
+    } catch (Exception $exception) {
+        return "Error: " . $exception->getMessage();
+    }
+
+}
+
+function signUp($username, $password)
+{
+    try {
+        $user = new User($username, $password);
+
+        if (find("users", "username", $user->username)) {
+            return "$username have already been used.";
+        }
+
+        $userID = createUser($user);
+
+        $_SESSION['userID'] = $userID;
+
+        header("Location: index.php");
+        exit();
+
+    } catch (Exception $exception) {
+        return "Error: " . $exception->getMessage();
+    }
+}
+
+function signIn($username, $password)
+{
+    try {
+
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $foundUser = find("users", "username", $username);
+
+        if ($foundUser == null) {
+            return "This user does not exist.";
+        }
+
+        if ($foundUser['password'] != $hashedPassword) {
+            return "Password is incorrect.";
+        }
+
+        $_SESSION['userID'] = $foundUser['userID'];
+
+        header("Location: index.php");
+        exit();
+
+    } catch (Exception $exception) {
+        return "Error: " . $exception->getMessage();
+    }
+}
+?>
