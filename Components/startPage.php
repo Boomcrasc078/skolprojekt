a22b51401f7b18d5fa98eb1ca89e67c3f8851f2e
CertifiedArabi <ramin.ghobadi96@gmail.com>
2026-03-09 12:24:55 +0100

    .

--- Components/startPage.php
+++ Components/startPage.php
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-<div class="mt-3 container">
-
-    <div class="px-4 py-5 my-5 text-center"> <img class="d-block mx-auto mb-4"
-            src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-        <h1 class="display-5 fw-bold text-body-emphasis">Quis, Study rehearsal made easy</h1>
-        <div class="col-lg-6 mx-auto">
-
-=======
 <div class="mt-3 container-fluid">
 
 
@@ -16,19 +7,10 @@
         <h1 class="display-5 fw-bold text-body-emphasis">Quis, Study rehearsal made easy</h1>
         <div class="col-lg-6 mx-auto">
 
->>>>>>> b7a191b486dfcae7100a57d8a41c5a7bb38cf75f
             <p class="lead mb-4">Make it easier to study and rehearse for tests with flashcards, quizzes and
                 more. All using Quis!
             </p>
 
-<<<<<<< HEAD
-            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center"> <button type="button"
-                    class="btn btn-primary btn-lg px-4 gap-3">Sign up</button> <button type="button"
-                    class="btn btn-outline-secondary btn-lg px-4">Log in</button>
-            </div>
-        </div>
-    </div>
-=======
             <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
                 <a href="signUp.php" class="btn btn-primary btn-lg px-4 gap-3">Sign up</a>
                 <a href="signIn.php" class="btn btn-outline-secondary btn-lg px-4">Sign In</a>
@@ -40,7 +22,7 @@
     </div>
     <!--Featured Title Showcase-->
     <div class="container px-4 py-5" id="featured-3">
-        <h2 class="pb-2 border-bottom">Columns with icons</h2>
+        <h2 class="pb-2 border-bottom">Features</h2>
         <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
             <div class="feature col">
                 <div
@@ -49,7 +31,7 @@
                         <use xlink:href="#collection"></use>
                     </svg>
                 </div>
-                <h3 class="fs-2 text-body-emphasis">Featured title</h3>
+                <h3 class="fs-2 text-body-emphasis">Flashcards</h3>
                 <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence
                     and probably just keep going until we run out of words.</p> <a href="#" class="icon-link">
                     Call to action
@@ -64,7 +46,7 @@
                         <use xlink:href="#people-circle"></use>
                     </svg>
                 </div>
-                <h3 class="fs-2 text-body-emphasis">Featured title</h3>
+                <h3 class="fs-2 text-body-emphasis">Study Sets</h3>
                 <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence
                     and probably just keep going until we run out of words.</p> <a href="#" class="icon-link">
                     Call to action
@@ -90,5 +72,4 @@
         </div>
     </div>
 
->>>>>>> b7a191b486dfcae7100a57d8a41c5a7bb38cf75f
 </div>
